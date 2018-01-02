9a4acf80b4823b0997cb5933b1e184adc6a5bb22
Christian Watts <civrev@gmail.com>
2018-01-02 11:31:12 -0500

    setup script for backup

---
+++ Raspbian/initial_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Running the initial setup for PiTutorials/Raspbian..."
+sudo apt-get update
+sudo apt-get update
+sudo apt-get dist-upgrade
+sudo apt-get dist-upgrade
+sudo apt-get install mysql-server
+sudo apt-get install git
+sudo apt-get install nginx
+sudo apt-get install python3-numpy
+sudo apt-get install python3-scipy
+sudo apt-get install python3-sklearn
+sudo apt-get install python3-matplotlib
+sudo apt-get install python3-pandas
+sudo apt-get install python3-seaborne
+sudo apt-get install python3-mysqlconnector
+sudo apt-get install python3-sqlalchemy
