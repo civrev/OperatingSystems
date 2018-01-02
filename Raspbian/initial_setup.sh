7afb1b1a6b5c7d53b6c3f9efb986f4e17cf4b8a1
Christian Watts <civrev@gmail.com>
2018-01-02 11:40:15 -0500

    Update initial_setup.sh

--- Raspbian/initial_setup.sh
+++ Raspbian/initial_setup.sh
@@ -4,14 +4,14 @@ sudo apt-get update
 sudo apt-get update
 sudo apt-get dist-upgrade
 sudo apt-get dist-upgrade
-sudo apt-get install mysql-server
-sudo apt-get install git
-sudo apt-get install nginx
-sudo apt-get install python3-numpy
-sudo apt-get install python3-scipy
-sudo apt-get install python3-sklearn
-sudo apt-get install python3-matplotlib
-sudo apt-get install python3-pandas
-sudo apt-get install python3-seaborne
-sudo apt-get install python3-mysqlconnector
-sudo apt-get install python3-sqlalchemy
+sudo apt-get -y install mysql-server
+sudo apt-get -y install git
+sudo apt-get -y install nginx
+sudo apt-get -y install python3-numpy
+sudo apt-get -y install python3-scipy
+sudo apt-get -y install python3-sklearn
+sudo apt-get -y install python3-matplotlib
+sudo apt-get -y install python3-pandas
+sudo apt-get -y install python3-seaborne
+sudo apt-get -y install python3-mysqlconnector
+sudo apt-get -y install python3-sqlalchemy
