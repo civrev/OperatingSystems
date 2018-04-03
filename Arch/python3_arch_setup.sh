114d5acc9b3b88a95cd093c8d255b7d8a0e568dd
civrev <civrev@gmail.com>
2018-04-03 00:56:38 +0000

    Very unhappy Arch work

--- Arch/python3_arch_setup.sh
+++ Arch/python3_arch_setup.sh
@@ -4,6 +4,10 @@
 #clear console
 clear
 
+echo "Getting pacman packages..."
+
+: <<'END'
+
 #get python
 echo y | pacman -S python
 #echo y | pacman -S tk #don't try these without a GUI
@@ -21,8 +25,45 @@ echo y | pacman -S python-statsmodels
 echo y | pacman -S python-seaborn
 echo y | pacman -S python-scikit-learn
 echo y | pacman -S python-tensorflow
-echo y | pacman -S python-pygame
 echo y | pacman -S python-h5py
-echo y | pacman -S python-keras
 
+END
+
+clear
+
+echo "Now Building AUR packages..."
+
+#Arch PKGBUILDs are community maintained
+#so priviledges magic will have to be performed to reduce permissions
+
+ADIR="$PWD"
+HDIR="/home/build"
+mkdir $HDIR
+chgrp nobody $HDIR
+chmod g+ws $HDIR
+setfacl -m u::rwx,g::rwx $HDIR
+setfacl -d --set u::rwx,g::rwx,o::- $HDIR
+
+#will avoid re-cloning git repositories
+#and will only call git pull
+
+#NOTE: This will never be true running this for the first time
+#cloned repositories will never be in THIS repository
+#and are handled by the gitignore
+
+#keras
+if [ ! -d "$HDIR/python-keras" ]; then
+	git clone https://aur.archlinux.org/python-keras.git $HDIR
+fi
+cd $HDIR/python-keras
+git pull
+sudo -u nobody makepkg -si
+
+#pygame
+if [ ! -d "$HDIR/python-pygame" ]; then
+	git clone https://aur.archlinux.org/python-pygame.git $HDIR
+fi
+cd $HDIR/python-pygame
+git pull
+sudo -u nobody makepkg -si
 
