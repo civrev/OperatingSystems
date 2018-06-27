b6e7ff2f0aa7e6a7f694ba812bba15da1790bb22
civrev <civrev@gmail.com>
2018-06-27 16:59:11 -0400

    more python

--- Ubuntu/python3.sh
+++ Ubuntu/python3.sh
@@ -8,7 +8,7 @@ echo "Now installing Python modules..."
 yes | sudo apt-get pip
 yes | sudo apt-get pip3
 
-#modules
+#modules using pip
 yes | sudo -H pip3 install numpy --upgrade
 yes | sudo -H pip3 install scipy --upgrade
 yes | sudo -H pip3 install matplotlib --upgrade
@@ -21,11 +21,15 @@ yes | sudo -H pip3 install pygame --upgrade
 yes | sudo -H pip3 install sqlalchemy --upgrade
 yes | sudo -H pip3 install selenium --upgrade
 
+#modules using apt
+yes | sudo add-apt-repository ppa:kivy-team/kivy
+yes | sudo apt-get install python3-kivy
+
 #build tensorflow from source...
 sudo bash tensorflow.sh
 
 #module that needs tensorflow
 yes | sudo -H pip3 install keras --upgrade
 
-#sublime text
+#sublime text for an IDE
 sudo bash sublime.sh
